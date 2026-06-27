b8aaae4b6a527d8263890cb29123483d7535cec6
dnnd99 <andhi99ka@gmail.com>
2026-06-27 09:57:47 +0700

    Create logger.py

---
+++ modules/logger.py
@@ -0,0 +1,47 @@
+import logging
+import sys
+from datetime import datetime
+
+class Logger:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._setup_logger()
+        return cls._instance
+    
+    def _setup_logger(self):
+        self.logger = logging.getLogger('PixelBoost')
+        self.logger.setLevel(logging.INFO)
+        
+        # Console handler
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(logging.INFO)
+        
+        # File handler
+        file_handler = logging.FileHandler(f'logs/pixelboost_{datetime.now().strftime("%Y%m%d")}.log')
+        file_handler.setLevel(logging.INFO)
+        
+        # Format
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+        
+        self.logger.addHandler(console_handler)
+        self.logger.addHandler(file_handler)
+    
+    def info(self, message):
+        self.logger.info(message)
+    
+    def error(self, message):
+        self.logger.error(message)
+    
+    def warning(self, message):
+        self.logger.warning(message)
+    
+    def debug(self, message):
+        self.logger.debug(message)
+
+# Singleton instance
+logger = Logger()
